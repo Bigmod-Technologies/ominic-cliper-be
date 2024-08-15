af98db5b80e90578c76693e0e1f17be2042cefec
enanalharun@gmail.com <enanalharun@gmail.com>
2024-08-15 15:32:16 +0600

    First Commit

---
+++ Omnic_Clipper/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Omnic_Clipper project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Omnic_Clipper.settings')
+
+application = get_wsgi_application()
