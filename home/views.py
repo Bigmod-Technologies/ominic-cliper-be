eeea4b8f2742be82571b5f645a3a168e79c57655
enanalharun@gmail.com <enanalharun@gmail.com>
2024-08-27 22:10:46 +0600

    All static HTML rendering complete
    # Sorted specific HTML files in their specific folder.
    # Added a base html for rendering other HTML.
    # Updated views.py and urls.py

--- home/views.py
+++ home/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    context = {'title': 'Omnic Clipper', 'show_contact_section': True}
+    return render(request, 'index.html', context)
+
+def blog(request):
+    context = {'title': 'Blog', 'show_contact_section': True}
+    return render(request, 'blog/blog.html', context)
+
+def blogDetails(request):
+    context = {'title': 'Blog Details', 'show_contact_section': True}
+    return render(request, 'blog/blogDetails.html', context)
+
+def termsOfService(request):
+    context = {'title': 'Terms of Service', 'show_contact_section': False, 'contact_url': True}
+    return render(request, 'others/405.html', context)
+
+def privacyPolicy(request):
+    context = {'title': 'Privacy Policy', 'show_contact_section': False, 'contact_url': True}
+    return render(request, 'others/406.html', context)
+
+def about(request):
+    context = {'title': 'About Us', 'show_contact_section': True}
+    return render(request, 'others/about.html', context)
+
+def error(request):
+    context = {'title': 'Error', 'show_contact_section': False, 'contact_url': True}
+    return render(request, 'others/error.html', context)
+
+def order(request):
+    context = {'title': 'Order', 'show_contact_section': False, 'contact_url': True}
+    return render(request, 'pricing/order.html', context)
+
+def pricingImage(request):
+    context = {'title': 'Pricing Image', 'show_contact_section': True}
+    return render(request, 'pricing/pricingImage.html', context)
+
+def service(request):
+    context = {'title': 'Service', 'show_contact_section': True}
+    return render(request, 'service/service.html', context)
+
+def serviceDetails(request):
+    context = {'title': 'Service Details', 'show_contact_section': True}
+    return render(request, 'service/serviceDetails.html', context)
+
+def servicePortfolio(request):
+    context = {'title': 'Service Portfolio', 'show_contact_section': True}
+    return render(request, 'service/servicePortfolio.html', context)
