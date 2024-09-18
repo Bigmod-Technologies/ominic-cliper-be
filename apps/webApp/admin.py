362f2865001a1a1946a6d9fa09d8db19c6c8c3fc
enanalharun@gmail.com <enanalharun@gmail.com>
2024-09-19 00:50:53 +0600

    Updated Price and Sample(Error in sample page)
    - Did not create any new app as I targeted to keep settings.py unchanged
    - Price page done
    - Problem in Sample page needs to be fixed
    - Updated Our status part in several html. (Still static but the information is now matching with home page)

--- apps/webApp/admin.py
+++ apps/webApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import PricingImage, Portfolio, PortfolioExampleImage
+
+
+@admin.register(PricingImage)
+class PricingImageAdmin(admin.ModelAdmin):
+    list_display = ('product_title', 'product_image', 'product_price', 'created_at', 'updated_at')
+    search_fields = ('image', 'product_title')
+    list_filter = ('product_title', 'created_at', 'updated_at')
+
+
+class PortfolioExampleImageInline(admin.TabularInline):
+    model = PortfolioExampleImage
+    extra = 1  # Allows adding more images in the admin panel
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('portfolio_title', 'portfolio_description')
+    inlines = [PortfolioExampleImageInline]  # Display example images inline
+
 
 # Register your models here.
