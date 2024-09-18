2de5200d175f51160b8982ee3a8480e31990c20d
Al Amin <almin.cont@gmail.com>
2024-09-18 12:03:07 +0600

    feat: test deploy

--- apps/webApp/views.py
+++ apps/webApp/views.py
@@ -1,38 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 from apps.service.models import Service
-from django.conf.urls import handler404
+from apps.testimonial.models import ClientTestimonial
+
 
 # Create your views here.
 def home(request):
-    service = Service.objects.all()
-    context = {'title': 'Omnic Clipper', 'show_contact_section': True, 'service': service}
-    return render(request, 'index.html', context)
+    service = Service.objects.all()[:8]
+    testimonial = ClientTestimonial.objects.all()[:5]
+    context = {
+        "title": "Omnic Clipper",
+        "show_contact_section": True,
+        "service": service,
+        "testimonials": testimonial,
+    }
+    return render(request, "index.html", context)
 
 
 def termsOfService(request):
-    context = {'title': 'Terms of Service', 'show_contact_section': False, 'contact_url': True}
-    return render(request, 'others/405.html', context)
+    context = {
+        "title": "Terms of Service",
+        "show_contact_section": False,
+        "contact_url": True,
+    }
+    return render(request, "others/405.html", context)
 
 
 def privacyPolicy(request):
-    context = {'title': 'Privacy Policy', 'show_contact_section': False, 'contact_url': True}
-    return render(request, 'others/406.html', context)
+    context = {
+        "title": "Privacy Policy",
+        "show_contact_section": False,
+        "contact_url": True,
+    }
+    return render(request, "others/406.html", context)
 
 
 def about(request):
-    context = {'title': 'About Us', 'show_contact_section': True}
-    return render(request, 'others/about.html', context)
+    context = {"title": "About Us", "show_contact_section": True}
+    return render(request, "others/about.html", context)
 
 
 def error(request):
-    return render(request, 'others/error.html', status=404)
+    return render(request, "others/error.html", status=404)
 
 
 def order(request):
-    context = {'title': 'Order', 'show_contact_section': False, 'contact_url': True}
-    return render(request, 'pricing/order.html', context)
+    context = {"title": "Order", "show_contact_section": False, "contact_url": True}
+    return render(request, "pricing/order.html", context)
 
 
 def pricingImage(request):
-    context = {'title': 'Pricing Image', 'show_contact_section': True}
-    return render(request, 'pricing/pricingImage.html', context)
+    context = {"title": "Pricing Image", "show_contact_section": True}
+    return render(request, "pricing/pricingImage.html", context)
