8ebcd6d84e2056002544a6f1ad5e52d968328d14
Al Amin <almin.cont@gmail.com>
2024-10-14 15:00:19 +0600

    feat: refesh token

--- apps/auth0/urls.py
+++ apps/auth0/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import (
-    UserLoginView,
-)
+from .views import UserLoginView, UserTokenVerifyView, UserRefreshTokenView
 
 
 # auth
 urlpatterns = [
     # URLs that do not require a session or valid token
     path("login/", UserLoginView.as_view(), name="login"),
+    path("token/verify/", UserTokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", UserRefreshTokenView.as_view(), name="token_refresh"),
 ]
--- apps/auth0/views.py
+++ apps/auth0/views.py
@@ -6,6 +6,8 @@ from drf_spectacular.utils import extend_schema
 from dj_rest_auth.views import (
     LoginView,
 )
+from rest_framework_simplejwt.views import TokenVerifyView
+from dj_rest_auth.jwt_auth import get_refresh_view
 
 # Create your views here.
 
@@ -14,3 +16,16 @@ from dj_rest_auth.views import (
 class UserLoginView(LoginView):
     def login(self):
         return super().login()
+
+
+@extend_schema(tags=["Auth"])
+class UserTokenVerifyView(TokenVerifyView):
+    pass
+
+
+refresh_view_class = get_refresh_view()
+
+
+@extend_schema(tags=["Auth"])
+class UserRefreshTokenView(refresh_view_class):
+    pass
